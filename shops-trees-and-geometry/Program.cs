eb24dc0d242828905e81292e958f845d0816d33a
Mat_Tol <matvey.talalaevskiy@gmail.com>
2023-05-27 19:54:54 +0300

    Haversine+linear solution

--- shops-trees-and-geometry/Program.cs
+++ shops-trees-and-geometry/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+Stopwatch sw = new Stopwatch();
+sw.Start();
+
+static void Main()
+{
+    Console.WriteLine("Введіть широту: ");
+    double lat1 = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Введіть довготу: ");
+    double lon1 = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Введіть радіус: ");
+    double radius = Convert.ToDouble(Console.ReadLine());
+
+    string[] lines = File.ReadAllLines("data.csv");
+    for (int i = 0; i < lines.Length; i++)
+    {
+        string[] data = lines[i].Split(';');
+        double lat2 = Convert.ToDouble(data[0]);
+        double lon2 = Convert.ToDouble(data[1]);
+        string type1 = data[2];
+        string type2 = data[3];
+        string name1 = data[4];
+        string name2 = data[5];
+
+        double distance = HaversineDistance(lat1, lon1, lat2, lon2);
+        if (distance <= radius)
+        {
+            Console.WriteLine("{0} {1} {2} {3} Haversine: {4}", type1, type2, name1, name2, distance);
+        }
+    }
+}
+
+static double HaversineDistance(double lat1, double lon1, double lat2, double lon2)
+{
+    double R = 6371;
+    double dLat = ToRadians(lat2 - lat1);
+    double dLon = ToRadians(lon2 - lon1);
+    lat1 = ToRadians(lat1);
+    lat2 = ToRadians(lat2);
+
+    double a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) + Math.Sin(dLon / 2) * Math.Sin(dLon / 2) * Math.Cos(lat1) * Math.Cos(lat2);
+    double d = 2 * R * Math.Asin(Math.Sqrt(a));
+    return d;
+}
+
+static double ToRadians(double angle)
+{
+    return Math.PI * angle / 180.0;
+}
+
+Main();
+
+sw.Stop();
+Console.WriteLine($"Elapsed time: {sw.Elapsed}");
